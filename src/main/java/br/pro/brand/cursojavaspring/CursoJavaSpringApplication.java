db7f4dfb3e1f2e1e1a7dd91d33cf69235ffb290b
Carlos Brand <carlosebrand@gmail.com>
2022-02-11 01:51:22 -0300

    Projeto Criado

---
+++ src/main/java/br/pro/brand/cursojavaspring/CursoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.pro.brand.cursojavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/pro/brand/demoFirstProject/DemoFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pro.brand.demoFirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
