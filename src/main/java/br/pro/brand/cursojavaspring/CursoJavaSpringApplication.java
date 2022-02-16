2bac8ffeeca1d14548a3118b548c1cd7afb9fc0c
Carlos Brand <carlosebrand@gmail.com>
2022-02-16 00:58:12 -0300

    add category on startup

--- src/main/java/br/pro/brand/cursojavaspring/CursoJavaSpringApplication.java
+++ src/main/java/br/pro/brand/cursojavaspring/CursoJavaSpringApplication.java
@@ -1,13 +1,30 @@
 package br.pro.brand.cursojavaspring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoJavaSpringApplication {
+import br.pro.brand.cursojavaspring.domain.Category;
+import br.pro.brand.cursojavaspring.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursoJavaSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJavaSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
