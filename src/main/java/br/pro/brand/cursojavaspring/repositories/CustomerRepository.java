d65a529902b51b5e4e3416b80d4ab3843ec7bf33
Carlos Brand <carlosebrand@gmail.com>
2022-02-22 00:31:49 -0300

    validate email repeated

--- src/main/java/br/pro/brand/cursojavaspring/repositories/CustomerRepository.java
+++ src/main/java/br/pro/brand/cursojavaspring/repositories/CustomerRepository.java
@@ -2,10 +2,14 @@ package br.pro.brand.cursojavaspring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.pro.brand.cursojavaspring.domain.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     
+    @Transactional(readOnly=true)
+	Customer findByEmail(String email);
+
 }
--- src/main/java/br/pro/brand/cursojavaspring/services/validation/CustomerInsertValidator.java
+++ src/main/java/br/pro/brand/cursojavaspring/services/validation/CustomerInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.pro.brand.cursojavaspring.domain.Customer;
 import br.pro.brand.cursojavaspring.domain.enums.CustomerType;
 import br.pro.brand.cursojavaspring.dto.CustomerInsertDTO;
+import br.pro.brand.cursojavaspring.repositories.CustomerRepository;
 import br.pro.brand.cursojavaspring.resources.exception.FieldMessage;
 import br.pro.brand.cursojavaspring.services.validation.utils.BR;
 
 public class CustomerInsertValidator implements ConstraintValidator<CustomerInsert, CustomerInsertDTO> {
+
+    @Autowired
+	private CustomerRepository rep;
 
     @Override
     public void initialize(CustomerInsert ann) {
@@ -31,6 +38,13 @@ public class CustomerInsertValidator implements ConstraintValidator<CustomerInse
                     && !BR.isValidCNPJ(objDto.getIdentification())) {
                 list.add(new FieldMessage("identification", "Invalid CNPJ"));
             }
+        }
+
+        if (objDto.getEmail() != null) {
+            Customer aux = rep.findByEmail(objDto.getEmail());
+		    if (aux != null) {
+	    		list.add(new FieldMessage("email", "Email already existing in database"));
+	    	}
         }
 
         for (FieldMessage e : list) {
